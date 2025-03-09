fd48d967cc26f435c108c22e05b19eec6fab447b
tschoolderman <t.d.schoolderman@gmail.com>
2025-03-09 16:33:58 +0100

    change menu and add icons

---
+++ Supermarket.Frontend/src/components/Landing.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+// import { onMounted } from 'vue'
+//
+// const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
+//
+//
+// const fetchData = async () => {
+//   debugger;
+//   try {
+//     const response = await fetch('https://localhost:7296/WeatherForecast'); // Example API
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok');
+//     }
+//     const jsonData = await response.json();
+//     console.log(jsonData);
+//   } catch (err) {
+//     // error.value = `Error: ${err.message}`; // Handle errors
+//     // console.error(err.message);
+//   } finally {
+//     // loading.value = false; // Data loading is complete
+//   }
+// }
+//
+// onMounted(fetchData())
+</script>
+
+<template>
+  <h1>basic landing</h1>
+  somewhere redirect to login page
+</template>
--- Supermarket.Frontend/src/views/HomeView.vue
+++ Supermarket.Frontend/src/views/HomeView.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import Landing from '../components/Landing.vue'
 
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Landing />
   </main>
 </template>
