f5dcf1363d68d2617cb27722d5bcbba440742c4b
bearclawvisions <t.d.schoolderman@gmail.com>
2025-05-27 16:24:27 +0200

    refactor landing to use basic container

--- Supermarket.Frontend/src/components/Landing.vue
+++ Supermarket.Frontend/src/components/Landing.vue
@@ -1,37 +1,32 @@
 <script setup lang="ts">
 import Panel from "primevue/panel";
+import BasicTextContainer from "@/components/containers/BasicTextContainer.vue";
 </script>
 
 <template>
-  <div class="basic-container">
-    <Panel>
-      <template #header>
-        <div>
-          <span class="pi pi-shopping-bag"/>
-          <span class="font-bold"> Supermarket</span>
-        </div>
-      </template>
-      <p>
-        The place that has all your daily needs!
-      </p>
-    </Panel>
-  </div>
+  <BasicTextContainer>
+    <template #icon>
+      <i class="pi pi-shopping-bag"/>
+    </template>
+    <template #title>
+      Supermarket
+    </template>
+    <template #body>
+      The place that has all your daily needs!
+    </template>
+  </BasicTextContainer>
   
-  <div class="basic-container">
-    <Panel header="Supermarket">
-      <template #header>
-        <div>
-          <span class="font-bold">Supermarket</span>
-        </div>
-      </template>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-        laborum.
-      </p>
-    </Panel>
-  </div>
+  <BasicTextContainer>
+    <template #title>
+      Supermarket
+    </template>
+    <template #body>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+      laborum.
+    </template>
+  </BasicTextContainer>
   
 </template>
