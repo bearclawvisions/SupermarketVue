86baf2bd976a9ad4c99b7af2f2e95feb35fda03b
bearclawvisions <t.d.schoolderman@gmail.com>
2025-06-08 17:41:51 +0200

    refactor basic and large text container

--- Supermarket.Frontend/src/components/containers/BasicTextContainer.vue
+++ Supermarket.Frontend/src/components/containers/BasicTextContainer.vue
@@ -3,21 +3,19 @@ import Panel from 'primevue/panel';
 </script>
 
 <template>
-  <div class="basic-container">
-    <Panel>
-      <template #header>
-        <div class="container-title">
-          <template v-if="$slots.icon">
-            <slot name="icon"/>&nbsp;
-          </template>
-          <slot name="title"/>
-        </div>
-      </template>
-      <template #default>
-        <slot name="body"/>
-      </template>
-    </Panel>
-  </div>
+  <Panel class="basic-container">
+    <template #header>
+      <div class="container-title">
+        <template v-if="$slots.icon">
+          <slot name="icon"/>&nbsp;
+        </template>
+        <slot name="title"/>
+      </div>
+    </template>
+    <template #default>
+      <slot name="body"/>
+    </template>
+  </Panel>
 </template>
 
 <style scoped>
--- Supermarket.Frontend/src/components/containers/LargeTextContainer.vue
+++ Supermarket.Frontend/src/components/containers/LargeTextContainer.vue
@@ -3,24 +3,22 @@ import Panel from 'primevue/panel';
 </script>
 
 <template>
-  <div class="large-text-container">
-    <Panel>
-      <template #header>
-        <div class="container-title">
-          <template v-if="$slots.icon">
-            <slot name="icon"/>&nbsp;
-          </template>
-          <slot name="title"/>
-          <div class="container-subtitle">
-            <slot name="subtitle"/>
-          </div>
+  <Panel class="large-text-container">
+    <template #header>
+      <div class="container-title">
+        <template v-if="$slots.icon">
+          <slot name="icon"/>&nbsp;
+        </template>
+        <slot name="title"/>
+        <div class="container-subtitle">
+          <slot name="subtitle"/>
         </div>
-      </template>
-      <template #default>
-        <slot name="body"/>
-      </template>
-    </Panel>
-  </div>
+      </div>
+    </template>
+    <template #default>
+      <slot name="body"/>
+    </template>
+  </Panel>
 </template>
 
 <style scoped>
