8f9ae55974084f7455bf5be7c6e3631b56d23f79
bearclawvisions <t.d.schoolderman@gmail.com>
2025-06-09 13:56:38 +0200

    sortable table and alternating row color

--- Supermarket.Frontend/src/components/tables/GenericDataTable.vue
+++ Supermarket.Frontend/src/components/tables/GenericDataTable.vue
@@ -1,22 +1,33 @@
 <script setup lang="ts" generic="T">
 import {Column, DataTable, Skeleton} from "primevue";
 import type {ColumnConfig} from "@/types/Models.ts";
+import {ref} from "vue";
 
 defineProps<{
   data: T[];
   columns: Array<ColumnConfig<T>>;
   isLoading?: boolean;
 }>();
+
+const selected = ref<T[]>([]);
 </script>
 
 <template>
-  <DataTable :value="data" class="datatable">
+  <DataTable
+      v-model:selection="selected"
+      selection-mode="single"
+      data-key="id"
+      :meta-key-selection="false"
+      :value="data" class="datatable"
+      striped-rows
+  >
     <Column
         v-for="column in columns"
         :key="String(column.field)"
         :field="String(column.field)"
         :header="column.header"
         :hidden="column.hidden"
+        :sortable="true"
     >
       <template #body="{ data }: { data: T }">
         <Skeleton v-if="isLoading" height="1.2rem"/>
